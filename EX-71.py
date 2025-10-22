445095e85e6b4a37901197f228c3b67012422ee1
mateus <mateuspontesdsilva11@gmail.com>
2025-10-22 17:34:23 -0300

    Create EX-71.py

---
+++ EX-71.py
@@ -0,0 +1,11 @@
+# @cikey eb34e327288c5f9052179536061c4901
+# @sid 20251174010031
+# @aid V7.1
+
+from string import ascii_lowercase
+
+#begin_inputs
+letras = ['m', 'b', 'f', 'j', 'a']
+#end_inputs
+
+
