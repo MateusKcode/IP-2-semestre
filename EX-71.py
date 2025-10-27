f6d7f99c18435ebba5737b946d1c50f69b547bec
mateus <mateuspontesdsilva11@gmail.com>
2025-10-27 18:21:21 -0300

    Update EX-71.py

--- EX-71.py
+++ EX-71.py
@@ -5,7 +5,16 @@
 from string import ascii_lowercase
 
 #begin_inputs
-letras = ['m', 'b', 'f', 'j', 'a']
+letras = ['a', 'b', 'c']
 #end_inputs
 
+from string import ascii_lowercase
+
+def letras_disponíveis(letras_usadas):
+    alfabeto = list(ascii_lowercase)
+    
+    disponiveis = [letra for letra in alfabeto if letra not in letras_usadas]
+    return disponiveis
+
+print(letras_disponíveis(letras))
 
