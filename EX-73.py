0c620a1f435d74089705c92e016034ef4b636524
mateus <mateuspontesdsilva11@gmail.com>
2025-11-04 13:51:28 -0300

    Update EX-73.py

---
+++ EX-73.py
@@ -0,0 +1,9 @@
+# @cikey eb34e327288c5f9052179536061c4901
+# @sid 20251174010031
+# @aid V7.3
+
+import random
+
+numeros_sorteados = random.sample(range(1, 40), 25)
+numeros_sorteados.sort()
+print(numeros_sorteados)
