e494ff4be370bf9c8e3f1c77d0c2bb7083f04e5a
mateus <mateuspontesdsilva11@gmail.com>
2025-11-11 13:32:30 -0300

    Update EX-74.py

---
+++ EX-74.py
@@ -0,0 +1,13 @@
+# @cikey eb34e327288c5f9052179536061c4901
+# @sid 20251174010031
+# @aid V7.4
+
+nome = []
+tel = []
+
+for n in nome:
+    user = input["Nome:"]
+    num = input["numero:"]
+    c = {user: num}
+    tel.append(c)
+print(f"{tel}")
